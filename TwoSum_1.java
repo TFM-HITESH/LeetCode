e2cbf5979ba9ad7ceba572fbeda56dd2b6304e0c
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-10-26 01:31:26 +0530

    TwoSum_1

--- TwoSum_1.java
+++ TwoSum_1.java
@@ -2,13 +2,10 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         TreeMap<Integer, Integer> numMap = new TreeMap<Integer, Integer>();
         int output[] = new int[2];
-
-        for(int i=0; i<nums.length; i++)
-        {
-            numMap.put(nums[i], i);
-        }
-
         int complement;
+
+        // in the intuitive two pass method, pass 1 is to put all the values into the map. Then find the complement and check if complement is withing the map
+
         for(int i=0; i<nums.length; i++)
         {
             complement = target-nums[i];
@@ -17,6 +14,9 @@ class Solution {
                 output[0] = i;
                 output[1] = numMap.get(complement);
             }
+            numMap.put(nums[i], i);
+
+            // in the one pass method, we first check if the complement is there, if not, just continue. We put the new value into the map anyways. If we dont find the partner the first time, no worries, we will find the original when we eventually loop till the the partner.
         }
 
         return output;
